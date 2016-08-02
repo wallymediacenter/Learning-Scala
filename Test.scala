4b0b47f686d519a5be09ed1f2c47a6401a19a248
wallymediacenter <mcassist@M1611673.local>
2016-08-02 16:32:12 -0400

    Back up

--- Test.scala
+++ Test.scala
@@ -1,4 +1,4 @@
-import scala.math._ //_ is the wildcard similar to * in java
+import scala.math._ /*_ is the wildcard similar to * in java*/
 
 object Test {
   def main(args : Array[String]) = {
@@ -19,6 +19,40 @@ object Test {
     val testInt:Double = 10.99;
     println("Convert Double to Integer " + testInt.toInt);
 
+    //Mathematical function
+    println("sqrt(2) =" + sqrt(2));
 
+    // Static method in java is similar to singleton object, and companion object
+
+    //Scala method can be created without () (a.k.a parentheses). METHOD that doesn't modify the object has no ()
+
+    //String
+    println("String as array" + "number4"(4)); //this is equivalent to "number4".charAt(4)
+
+    //The use if BigInt
+
+    val twoPow = BigInt(2);
+    println("2^1024 = " + twoPow.pow(1024));
+
+    //Class Unit that have one value (), Unit ~ void in java. () is a place holder for "no useful value"
+
+    val example = 10 + //
+    11
+    println("10 + 11 = " + example); //Use // to connect the next line
+
+    //{Block} contains a sequence of expression, and the result is also an expression
+    val distance = {
+      val dx = 10;
+      val dy = 20;
+      sqrt(dx*dx + dy*dy); //Value of distance is the last expression
+    }
+
+    println("Distance " + distance);
+
+    //For loop
+    for(i <- 1 to 10) println("Loop #:" + i);
+
+    val s = "Traverse";
+    for(i <- 0 until s.length) println("Letter Counting: " + s(i));
   }
 }
