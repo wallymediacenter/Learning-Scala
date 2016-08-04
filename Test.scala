067788dbee096c05d2783580a2d8e95be1783f08
wallymediacenter <mcassist@M1611673.local>
2016-08-04 16:30:46 -0400

    Back up

--- Test.scala
+++ Test.scala
@@ -1,23 +1,34 @@
 object Test{
   def main(args : Array[String]) : Unit = {
-    val newClass = new MyClass()
+    val newClass = new MyClass("Awesome", 100)
     println(newClass.name) //Getter
     newClass.name_=("New Name") //Setter
     println(newClass.name)
+
+    val newClass2 = new MyClass("A little bit Awesome")
+    println(newClass2.name) //Getter
+    println(newClass2.value) //Getter
   }
 
 }
 
 //CLass
-class MyClass{
+//PRIMARY constructor is written wth class defnition
+class MyClass(var name: String, var value : Int){
   //Fields | MUST INITIALIZE the fields
-  var name : String = "Null"
-  var value : Int = 0
+
   private[this] var secret = "MESSAGE" ////Object-private only accessible through that specific object. NO getter and setter
+  //THE PRIMARY construcor executes all statements in the class deffinition
   //methods are public by default
   //getter . || setter _=()
 
-  //Auxiliary constructor
+  //Auxiliary constructor || Similar to constructor in Java, must start with a call to a previously definex ausliary constructor or the primary constructor
+  def this(newName : String) {
+    this(newName, 0) //MUST HAVE
+    this.name = newName
+  }
+
+
 
 
 }
